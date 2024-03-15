805c8b6c51ef3538e39fec0234abec7ec0fb6a6a
Ulfin85 <92341475+Ulfin85@users.noreply.github.com>
2024-03-15 10:54:51 -0400

    Fixed Push

--- Assets/Scripts/WorkspaceHandler.cs
+++ Assets/Scripts/WorkspaceHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnterWorkSpace : MonoBehaviour
+public class WorkspaceHandler : MonoBehaviour
 {
     private bool inWorkspace = false;
     public MeshRenderer plane;
